57591de1d112df444a70668e8ef840cf50437937
Dor Zhavian <dorzhavian@gmail.com>
2024-08-30 19:58:17 +0300

    Added flash animation with jQueiry and Audio to buttons

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("CHECK")
+var buttonColors = ["red", "green", "yellow", "blue"];
+var gamePattren = [];
+
+function nextSequence() {
+    var randomChosenNum = Math.floor(Math.random() * 6) + 1
+    var randomChosenColor = buttonColors[randomChosenNum];
+    gamePattren.push(randomChosenColor);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
