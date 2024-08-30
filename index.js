e4ae2d2940987e53693c5f170470b37993c97933
Dor Zhavian <dorzhavian@gmail.com>
2024-08-30 20:09:32 +0300

    Add sounds for buttons , arrange and change names of vars

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var buttonColors = ["red", "green", "yellow", "blue"];
-var gamePattren = [];
 
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
 var userClickedPattern = [];
 
 $(".btn").click(function() {
+
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
 });
 
 function nextSequence() {
-    var randomChosenNum = Math.floor(Math.random() * 6) + 1
-    var randomChosenColor = buttonColors[randomChosenNum];
-    gamePattren.push(randomChosenColor);
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
